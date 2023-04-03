a0fbd99b9b6e4a872dbd148f4abf7944cba4a6d0
joseph selva <daymoove@gmail.com>
2023-04-03 16:36:43 +0200

    modif backup script

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rsync -avz [user]@[ip]:/[source] /[destination]
-sudo tar -cvzf home.tar.gz /[source]
-sudo rm -r /[source]
+sudo tar -cvzf backup.tar.gz /[destination]/[source]
+sudo rm -r /[destination]/[source]
