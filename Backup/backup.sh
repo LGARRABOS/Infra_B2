4949327c7763b2279167f750d07ef7b2345e705a
joseph selva <daymoove@gmail.com>
2023-04-03 17:55:32 +0200

    modif backup script

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -2,6 +2,6 @@
 
 file_date=$(date +"%y%m%d_%H%M%S")
 
-rsync -avz [user]@[ip]:/[source] /[destination]
-tar -cvzf /[destination]/"${file_date}".tar.gz /[destination]/[source]
-rm -r /[destination]/[source]
+rsync -avz [user]@[ip]:/[source] /[home_user_backup]
+tar -cvzf /[home_user_backup]/"${file_date}".tar.gz /[home_user_backup]/[source]
+rm -r /[home_user_backup]/[source]
